59cc676b7eac6b1ebfb414d868f9e1aad53af427
Roberto Previdi <hariseldon78@gmail.com>
2017-03-16 17:25:03 +0100

    progress type support in restengine

--- APIUtils/RestEngine.swift
+++ APIUtils/RestEngine.swift
@@ -14,6 +14,7 @@ import Curry
 import Runes
 import RxSwift
 import Cartography
+import DataVisualization
 
 /////////////////////
 ////////////////////////
@@ -28,21 +29,19 @@ public enum RestApiFlags {
 public protocol RestApi:Tagged {
 	associatedtype In:Encodable
 	var input:In {get}
-	var view:UIView? {get}
+	var progressType:ProgressType? {get}
 	var debugLevel:APIDebugLevel {get}
 	var url:String {get}
 	var method:Alamofire.HTTPMethod {get}
 	var encoding:Alamofire.ParameterEncoding {get}
 	var flags:Set<RestApiFlags> {get}
-	var activityHandlerStyle:UIActivityIndicatorViewStyle {get}
 }
 
 public extension RestApi {
-	var view:UIView? {return nil}
+	var progressType:ProgressType? {return nil}
 	var debugLevel:APIDebugLevel {return .verbose}
 	var flags:Set<RestApiFlags> {return Set<RestApiFlags>()}
 	var logTags:[String] {return ["api",Self.tag]}
-	var activityHandlerStyle:UIActivityIndicatorViewStyle {return .gray}
 	var encoding:Alamofire.ParameterEncoding {
 		if method==HTTPMethod.get {
 			return URLEncoding(destination: .methodDependent)
@@ -58,42 +57,36 @@ public protocol ArrayApi:RestApi {
 	associatedtype Out:Arrayable
 }
 extension RestApi {
-	func makeRequest(debugCallId:Int)->(Result<DataRequest>,UIProgressView?)
+	func makeRequest(debugCallId:Int)->Result<DataRequest>
 	{
 
 		var jsonDict:[String : ApiParam]?=nil
 		if !flags.contains(.emptyBody) && method != HTTPMethod.get {
 			guard let dict=input.encode().JSONObject() as? [String:Any] else {
-				return (.failure(NSError(domain: "no dictionary in request", code: 0, userInfo: nil)),nil)
+				return .failure(NSError(domain: "no dictionary in request", code: 0, userInfo: nil))
 			}
 			jsonDict=dict.typeConstrain()
 		}
-		let req=MunicipiumAPIAlamofire
-			.request(url, method: method, parameters: jsonDict, encoding: encoding, headers: nil)
-			.debugLog(debugCallId:debugCallId, debugLevel:debugLevel,logTags:logTags, params:jsonDict)
-		let success = Result<DataRequest>.success(req)
-		var progressView:UIProgressView?=nil
-		if let v=self.view, success.isSuccess {
-			progressView=UIProgressView(progressViewStyle: .default)
-			progressView?.tintColor = .purple
-			onMain{
-				v.addSubview(progressView!)
-				constrain(progressView!) {
-					let sv=$0.superview!
-					$0.top == sv.top
-					$0.trailing == sv.trailing
-					$0.leading == sv.leading
-				}
-				progressView!.setContentCompressionResistancePriority(1000, for: UILayoutConstraintAxis.vertical)
-			}
-			req.downloadProgress { (progress) in
-				progressView!.setProgress(
-					Float(progress.fractionCompleted),
-					animated: true)
-			}
+		
+		var req:Alamofire.DataRequest!
+		if encoding is JSONEncoding {
+			
+			let jsonData=try! JSONSerialization.data(withJSONObject: jsonDict ?? [:], options: .prettyPrinted)
+			req=MunicipiumAPIAlamofire
+				.upload(jsonData, to: url, method: method, headers: ["Content-type":"application/json"])
+				.uploadProgress(closure: { (prog) in
+					self.progressType?.setCompletion(CGFloat(prog.fractionCompleted))
+				})
+		} else {
+			req=MunicipiumAPIAlamofire
+				.request(url, method:method, parameters:jsonDict, encoding:encoding, headers:nil)
 		}
 		
-		return (success,progressView)
+		req=req.debugLog(debugCallId:debugCallId, debugLevel:debugLevel,logTags:logTags, params:jsonDict)
+
+		let success = Result<DataRequest>.success(req)
+
+		return success
 	}
 }
 public extension ObjApi {
@@ -105,7 +98,14 @@ public extension ObjApi {
 		
 		return Observable.create{ (observer) -> Disposable in
 			let start=Date()
-			let (request,progressView)=self.makeRequest(debugCallId:debugCallId)
+			let actInd=UIApplication.shared.activityHandler(style: UIActivityIndicatorViewStyle.white)
+			var done=false
+			delay(0.5) {
+				if !done {
+					self.progressType?.start()
+				}
+			}
+			let request=self.makeRequest(debugCallId:debugCallId)
 			guard let req=request.value else {
 				observer.onError(request.error!)
 				return Disposables.create()
@@ -113,8 +113,10 @@ public extension ObjApi {
 
 			req.responseJSON { response in
 				defer {
-					progressView?.removeFromSuperview()
 					log("[\(debugCallId)]call duration: \(Date().timeIntervalSince(start))",self.logTags,.verbose)
+					actInd.hide()
+					done=true
+					self.progressType?.finish()
 				}
 				guard let j = response.result.value else {
 					observer.onError(NSError(domain: "Invalid json: \(response.result.value)", code: 0, userInfo: nil))
@@ -134,7 +136,11 @@ public extension ObjApi {
 				
 			}
 			return Disposables.create {
+				guard !done else {return}
+				done=true
+				actInd.hide()
 				req.cancel()
+				self.progressType?.cancel()
 			}
 			}.retryWhen { (errors: Observable<NSError>) in
 				return errors.scan(0) { ( a, e) in
@@ -158,7 +164,7 @@ public extension ArrayApi {
 		
 		return Observable.create{ (observer) -> Disposable in
 			let start=Date()
-			let (request,progressView)=self.makeRequest(debugCallId:debugCallId)
+			let request=self.makeRequest(debugCallId:debugCallId)
 			guard let req=request.value else {
 				observer.onError(request.error!)
 				return Disposables.create()
@@ -166,7 +172,6 @@ public extension ArrayApi {
 			
 			req.responseJSON { response in
 				defer {
-					progressView?.removeFromSuperview()
 					log("[\(debugCallId)]call duration: \(Date().timeIntervalSince(start))",self.logTags,.verbose)
 				}
 				log("timeline: \(response.timeline)",self.logTags,.verbose)
@@ -275,14 +280,14 @@ open class RestApiBase<_In,_Out> {
 	public typealias In = _In
 	public typealias Out = _Out
 	public let input:In
-	public let view:UIView?
-	public init(input:In, view:UIView?) {
+	public let progressType:ProgressType?
+	public init(input:In, progressType:ProgressType?) {
 		self.input=input
-		self.view=view
+		self.progressType=progressType
 	}
 	// default argument not working... (crash the compiler)
 	public init(input:In) {
 		self.input=input
-		self.view=nil
+		self.progressType=nil
 	}
 }
